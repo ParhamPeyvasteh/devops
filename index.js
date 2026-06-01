a48a53fbeb406b31f7eb0980d9febb7dd84c73ab
parham <mrprpy@gmail.com>
2026-06-01 22:42:36 +0330

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Devops 1");
+console.log("Github Actions CI/CD");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Test started ...");
+setTimeout(() => console.log("Wiating for reasults"),3000);
+console.log("All Done");
